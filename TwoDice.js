bbbce9da02d7688fa352e8fc208a3f36e516d276
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-26 01:16:16 +0530

    Random two dice numbers

---
+++ TwoDice.js
@@ -0,0 +1,7 @@
+const dice1 = 0;
+const dice2 = 0;
+let randomDiceValue1 = Math.floor(Math.random() * 10) % 6 + 1;
+let randomDiceValue2 = Math.floor(Math.random() * 10) % 6 + 1;
+
+console.log("Dice One : "+ randomDiceValue1);
+console.log("Dice Two : "+ randomDiceValue2);
