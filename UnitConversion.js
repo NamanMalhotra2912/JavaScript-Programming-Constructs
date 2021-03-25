a5ac319496dc577ed474e1a08b7ff29974aa81d9
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-26 01:32:29 +0530

    Area of plot

---
+++ UnitConversion.js
@@ -0,0 +1,10 @@
+let ft = 1;
+let inch = 42;
+
+let calculateInch = inch /12;
+
+let plotSize = 60*40;
+let numberOfPlots = 25;
+
+console.log("Ft will be : "+ calculateInch);
+console.log("Area of plots will be : "+ plotSize * numberOfPlots);
